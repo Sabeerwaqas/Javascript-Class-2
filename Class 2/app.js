00819ece72471861eb19ea91195fa4c3f4ab9bde
Sabeer Waqas <71982222+Sabeerwaqas@users.noreply.github.com>
2022-05-18 10:58:06 +0500

    Add files via upload

---
+++ Class 2/app.js
@@ -0,0 +1,59 @@
+
+// Write a program that calculates marks of subjects.
+
+
+// var html = +prompt("Enter HTML Marks");
+// var css  = +prompt("Enter css Marks");
+
+// var totalObtain = html + css;
+
+// var totalNum   = 200;
+
+// var percentage = totalObtain/totalNum*100
+
+// console.log(percentage + "%");
+
+
+
+// var dish = prompt("Whta is your favorite dish");
+
+// alert("Here is your " + dish);
+
+
+// Function
+
+// function calcTotalPercent(){
+
+//     var html = +prompt("Enter HTML Marks");
+// var css  = +prompt("Enter css Marks");
+
+// var totalObtain = html + css;
+
+// var totalNum   = 200;
+
+// var percentage = totalObtain/totalNum*100
+
+// console.log(percentage + "%");
+
+// }
+
+
+// calcTotalPercent();
+
+
+
+function sum (value1, value2){
+
+    var value1 = +prompt("Enter your 1st value");
+
+    var value2 = +prompt("Enter your 2nd value");
+
+    var Total = value1 + value2;
+
+    console.log(Total);
+
+}
+
+
+
+
